cae735ca5d161af931bd63e64b44ee8e0ae9ac38
umayanganacooray <ume.cooray@gmail.com>
2024-10-12 15:49:58 +0530

    Initial commit

---
+++ src/main/java/com/example/user_management_backend/UserManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_management_backend/UserManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
